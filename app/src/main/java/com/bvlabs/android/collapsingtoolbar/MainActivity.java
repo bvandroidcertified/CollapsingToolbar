809d23f8290f1bd91d761543774548ef85a690d9
bvandroidcertified <mbaiwodjibienvenuedev@gmail.com>
2019-09-07 02:33:21 +0200

    Initial commit

---
+++ app/src/main/java/com/bvlabs/android/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,34 @@
+package com.bvlabs.android.collapsingtoolbar;
+
+import android.support.design.widget.BaseTransientBottomBar;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar=(Toolbar)findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        fab=(FloatingActionButton)findViewById(R.id.fab_id);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Floating Action Button", BaseTransientBottomBar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
